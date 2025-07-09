1ae3a424cb2360d74fa0e98db2eced7076d28aa5
arhixia <fedyushinvovan@gmail.com>
2025-07-09 17:26:28 +0500

    сделал авторизацию и тесты на них

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+from backend.database.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
